581421eb9eac09d22bf908732163aa8d9a25f358
Timothy Davenport <tmthydvnprt@users.noreply.github.com>
2015-05-24 14:56:19 -0700

    turned cookie in localStorage

---
+++ source/assets/js/cookieJar.js
@@ -0,0 +1,38 @@
+var cookieJar = {
+    get: function (key) {
+        if (!key) {
+            return null;
+        } else {
+            return localStorage.getItem(key);
+        }
+    },
+    set: function (key, value) {
+        if (!key) {
+            return false;
+        } else {
+            return localStorage.setItem(key, value);
+        }
+    },
+    remove: function (key) {
+        if (!this.has(key)) {
+            return false;
+        } else {
+            return localStorage.removeItem(key);
+        }
+    },
+    has: function (key) {
+        if (!key) {
+            return false;
+        } else {
+            return $.inArray(key, this.cookies()) > -1;
+        }
+    },
+    cookies: function () {
+        var keys = [],
+            key;
+        for (key in localStorage) {
+            keys.push(key);
+        }
+        return keys;
+    }
+};

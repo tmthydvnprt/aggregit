916270711a5c1189be993e0c539cdd50db21572a
tmthydvnprt <tim@tmthydvnprt.com>
2016-02-12 05:56:26 -0800

    debug failed responses

--- output/js/github.js
+++ output/js/github.js
@@ -170,7 +170,10 @@ var github = {
             console.log('Has Access Token, check if still valid');
 
             // Check rate
-            $.when(this.request_handler('rate_limit')).done(function (rate_limit) {
+            $.when(this.request_handler('rate_limit')).always(function (rate_limit, status, xhr) {
+                console.log(rate_limit);
+                console.log(status);
+                console.log(xhr);
                 console.log('Rate Limit request done');
                 if (rate_limit["message"] === "Bad credentials") {
                     console.log('Token is not valid');

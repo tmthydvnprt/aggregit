641477f3e3abf8bdb750c9828ba6254b57d4494c
tmthydvnprt <tim@tmthydvnprt.com>
2016-02-14 11:12:31 -0800

    publish from last add-authentication commit: linting github.js

--- js/github.js
+++ js/github.js
@@ -1,11 +1,11 @@
-/*globals $,console,d3,cookieJar */
+/*globals $,console,d3,cookieJar,github,deparam,unurl,copyBIfInA,parse_headers */
 
 /*!
  * github.js
  *
  * Copyright 2016 Timothy Davenport; Licensed MIT
  */
- (function () {
+(function () {
     'use strict';
     window.github = {
         // API Defaults and Constants
@@ -73,9 +73,9 @@
 
         // Authorize and Authenticate
         //--------------------------------------------------------------------------------------------------------------------------
-        authorize : function() {
+        authorize : function () {
             var url = '';
-            this.state = Math.random().toString(36).substr(2, 8) + Math.random().toString(36).substr(2, 8)
+            this.state = Math.random().toString(36).substr(2, 8) + Math.random().toString(36).substr(2, 8);
             // store state in cookie for later
             cookieJar.set('state', this.state);
             // Create url to access GitHub authentication
@@ -90,24 +90,24 @@
             console.log(url);
             location.href = url;
         },
-        authenticate : function() {
+        authenticate : function () {
             console.log('Getting GitHub Authentication');
             // Get GitHub authentication from redirected url
             var auth = deparam(window.location.search),
                 url = '',
                 username = '';
             // Check that state is valid
-            if (cookieJar.get('state') === auth['state'] ) {
+            if (cookieJar.get('state') === auth.state) {
                 console.log('state is good');
                 // Turn authorization code into access token
                 url = this.oauth_proxy_url + $.param(auth);
-                $.getJSON(url, function(access) {
+                $.getJSON(url, function (access) {
                     if (access.hasOwnProperty('access_token')) {
                         console.log('token is good');
                         console.log('authenticated');
                         // Store auth access
-                        github.access_token = access['access_token'];
-                        cookieJar.set('access_token', access['access_token']);
+                        github.access_token = access.access_token;
+                        cookieJar.set('access_token', access.access_token);
                         cookieJar.set('valid_auth', true);
                         cookieJar.set('auth_time', (new Date()).toISOString());
                         // if user was begining a search before authenticating, send them back to their search
@@ -124,7 +124,7 @@
                 location.href = location.href.replace(location.search, '').replace(location.hash, '') + '#!/home';
             }
         },
-        check_authentication : function(callback) {
+        check_authentication : function (callback) {
             // Uses the rate_limit API to check if user authorization is still valid
             console.log('Checking GitHub Authorization');
 
@@ -142,7 +142,7 @@
                     }
 
                     console.log('Rate Limit request done');
-                    if (status === 'error' && data["message"] === "Bad credentials") {
+                    if (status === 'error' && data.message === 'Bad credentials') {
                         console.log('Token is not valid');
                         console.log(data);
                         cookieJar.set('valid_auth', false);
@@ -165,7 +165,7 @@
         // API Access
         //--------------------------------------------------------------------------------------------------------------------------
         // Request Handler
-        request_handler : function(request) {
+        request_handler : function (request) {
             // Split of argument object into array for arguments after request
             var args = Array.prototype.splice.call(arguments, 1),
                 // Pass argument array into requesting url function
@@ -179,14 +179,14 @@
                 return $.ajax({
                     dataType: "json",
                     url: url
-                })
+                });
             } else {
                 console.log('Not enough API calls left. Reset at {0}'.format(new Date(this.rate_limit_reset * 1000)));
                 return false;
             }
         },
         // Response Handler
-        response_handler : function(data, status, xhr) {
+        response_handler : function (data, status, xhr) {
 
             // If error, move things around because github errors still contain useful data
             if (status === 'error') {
@@ -222,7 +222,7 @@
                     route_log += ' Repo Language';
                 } else if (request_url.match('https://api.github.com/repos/.*/.*/stat')) {
                     route_log += ' Repo Stat';
-                } else if (request_url.match('https://api.github.com/repos/') ) {
+                } else if (request_url.match('https://api.github.com/repos/')) {
                     route_log += ' Repo';
                 } else {
                     route_log += ' Unknown Type';
@@ -255,11 +255,11 @@
         // API request urls
         //--------------------------------------------------------------------------------------------------------------------------
         // build params, starts with access_token if it exists then extends with other_params if neccesary
-        build_params : function(other_params) {
+        build_params : function (other_params) {
             var params = {};
             // add access_token to params if it exists
             if (this.access_token !== '') {
-                params['access_token'] = this.access_token;
+                params.access_token = this.access_token;
             }
             // extend params
             $.extend(params, other_params);
@@ -280,11 +280,11 @@
             current_user_repositories : function (type, page, per_page, sort) {
                 // https://api.github.com/user/repos{?type,page,per_page,sort}
                 var url = '',
-                params = {};
-                if (type) { params['type'] = type; }
-                if (page) { params['page'] = page; }
-                if (per_page) { params['per_page'] = per_page; }
-                if (sort) { params['sort'] = sort; }
+                    params = {};
+                if (type) { params.type = type; }
+                if (page) { params.page = page; }
+                if (per_page) { params.per_page = per_page; }
+                if (sort) { params.sort = sort; }
                 url = [this.api_url, 'user', 'repos'].join('/') + this.build_params();
                 return url;
             },
@@ -296,11 +296,11 @@
             user_repositories : function (user, type, page, per_page, sort) {
                 // https://api.github.com/users/{user}/repos{?type,page,per_page,sort}
                 var url = '',
-                params = {};
-                if (type) { params['type'] = type; }
-                if (page) { params['page'] = page; }
-                if (per_page) { params['per_page'] = per_page; }
-                if (sort) { params['sort'] = sort; }
+                    params = {};
+                if (type) { params.type = type; }
+                if (page) { params.page = page; }
+                if (per_page) { params.per_page = per_page; }
+                if (sort) { params.sort = sort; }
                 url = [this.api_url, 'users', user, 'repos'].join('/') + this.build_params();
                 return url;
             },
@@ -353,18 +353,18 @@
                 // https://api.github.com/gists/starred
                 var url = [this.api_url, 'gists', 'starred'].join('/') + this.build_params();
                 return url;
-            },
+            }
         },
 
         // API requests
         //--------------------------------------------------------------------------------------------------------------------------
         // Auth User
-        get_current_user : function(callback) {
-            $.when(this.request_handler('current_user')).always(this.response_handler).done(function(user_data) {
+        get_current_user : function (callback) {
+            $.when(this.request_handler('current_user')).always(this.response_handler).done(function (user_data) {
                 // Grab only the data we need
                 var user = copyBIfInA(github.user_keys, user_data);
                 // Store Data
-                github.data['auth_user'] = user;
+                github.data.auth_user = user;
                 cookieJar.set('auth_user', user);
                 // Send back data
                 if (callback) {
@@ -372,8 +372,8 @@
                 }
             });
         },
-        current_user_repos : function(callback) {
-            $.when(this.request_handler('current_user_repositories')).always(this.response_handler).done(function(repo_data) {
+        current_user_repos : function (callback) {
+            $.when(this.request_handler('current_user_repositories')).always(this.response_handler).done(function (repo_data) {
                 // Grab only the data we need
                 var repo = copyBIfInA(github.repo_keys, repo_data);
                 // Send back data
@@ -383,16 +383,16 @@
             });
         },
         // Queried User
-        get_user : function(user, callback) {
+        get_user : function (user, callback) {
             user = unurl(user);
-            $.when(this.request_handler('user', user)).always(this.response_handler).done(function(user_data) {
+            $.when(this.request_handler('user', user)).always(this.response_handler).done(function (user_data) {
                 // Grab only the data we need
                 var user = copyBIfInA(github.user_keys, user_data);
                 // Store Data
                 if (github.data.hasOwnProperty('user')) {
                     $.extend(true, github.data.user, user);
                 } else {
-                    github.data['user'] = user;
+                    github.data.user = user;
                 }
                 // Send back data
                 if (callback) {
@@ -400,9 +400,9 @@
                 }
             });
         },
-        get_user_repos : function(user, callback) {
+        get_user_repos : function (user, callback) {
             user = unurl(user);
-            $.when(this.request_handler('user_repositories', user)).always(this.response_handler).done(function(repos_data) {
+            $.when(this.request_handler('user_repositories', user)).always(this.response_handler).done(function (repos_data) {
                 var repos = [];
                 // Loop thru repos
                 repos_data.forEach(function (repo_data, i) {
@@ -413,7 +413,7 @@
                 if (github.data.user.hasOwnProperty('repo_list')) {
                     github.data.user.repo_list.push.apply(github.data.user.repo_list, repos);
                 } else {
-                    github.data.user['repo_list'] = repos;
+                    github.data.user.repo_list = repos;
                 }
                 // Send back data
                 if (callback) {
@@ -421,43 +421,41 @@
                 }
             });
         },
-        get_repo : function(owner, repo, callback) {
+        get_repo : function (owner, repo, callback) {
             owner = unurl(owner);
             repo = unurl(repo);
-            $.when(this.request_handler('repository', owner, repo)).always(this.response_handler).done(function(repo_data) {
+            $.when(this.request_handler('repository', owner, repo)).always(this.response_handler).done(function (repo_data) {
                 // Grab only the data we need
                 var repo = copyBIfInA(github.repo_keys, repo_data),
-                    name = repo.name;
+                    name = repo.name,
+                    obj = {};
+                // Package data
+                obj = {'repos' : {}};
+                obj.repos[name] = repo;
                 // Store Data
-                $.extend(true, github.data.user, {
-                    'repos' : {
-                        [name] : repo
-                    }
-                });
+                $.extend(true, github.data.user, obj);
                 // Send back data
                 if (callback) {
                     callback(repo);
                 }
             });
         },
-        get_repo_lang : function(owner, repo, callback) {
+        get_repo_lang : function (owner, repo, callback) {
             owner = unurl(owner);
             repo = unurl(repo);
-            $.when(this.request_handler('repository_languages', owner, repo)).always(this.response_handler).done(function(repo_lang_data) {
+            $.when(this.request_handler('repository_languages', owner, repo)).always(this.response_handler).done(function (repo_lang_data) {
                 // Determine which repo it came from based on url
-                var match = this.url.match(/https:\/\/api.github.com\/repos\/.*\/(.*)\/languages\?access_token=.*/),
-                    name = '';
+                var match = this.url.match(/https:\/\/api\.github\.com\/repos\/\w[\w\-]+\/(\w[\w\-_.]+)\/languages\?access_token=[\w]*/),
+                    name = '',
+                    obj = {};
 
                 if (match && match.length > 1) {
                     name = match[1];
+                    // Package data
+                    obj = {'repos' : {}};
+                    obj.repos[name] = {'languages' : repo_lang_data};
                     // Store Data
-                    $.extend(true, github.data.user, {
-                        'repos' : {
-                            [name] : {
-                                'languages' : repo_lang_data
-                            }
-                        }
-                    });
+                    $.extend(true, github.data.user, obj);
                 } else {
                     console.log('Bad url parse. Couldn\'t get repo name.');
                 }
@@ -467,29 +465,26 @@
                 }
             });
         },
-        get_repo_stat : function(owner, repo, stat, callback) {
+        get_repo_stat : function (owner, repo, stat, callback) {
             owner = unurl(owner);
             repo = unurl(repo);
             stat = unurl(stat);
-            $.when(this.request_handler('repository_stats', owner, repo, stat)).always(this.response_handler).done(function(repo_stat_data) {
+            $.when(this.request_handler('repository_stats', owner, repo, stat)).always(this.response_handler).done(function (repo_stat_data) {
                 // Determine which repo it came from based on url
-                var match = this.url.match(/https:\/\/api.github.com\/repos\/.*\/(.*)\/stats\/(.*)\?access_token=.*/),
+                var match = this.url.match(/https:\/\/api\.github\.com\/repos\/\w[\w\-]+\/(\w[\w\-_.]+)\/stats\/([\w]*)\?access_token=[\w]*/),
                     name = '',
-                    stat = '';
+                    stat = '',
+                    obj = {};
 
                 if (match && match.length > 1) {
                     name = match[1];
                     stat = match[2];
+                    // Package data
+                    obj = {'repos' : {}};
+                    obj.repos[name] = {'stats' : {}};
+                    obj.repos[name].stats[stat] = repo_stat_data;
                     // Store Data
-                    $.extend(true, github.data.user, {
-                        'repos' : {
-                            [name] : {
-                                'stats' : {
-                                    [stat] : repo_stat_data
-                                }
-                            }
-                        }
-                    });
+                    $.extend(true, github.data.user, obj);
                 } else {
                     console.log('Bad url parse. Couldn\'t get repo name.');
                 }
@@ -500,29 +495,29 @@
             });
         },
         // Try to get everything public from a queried user
-        get_all_user_data : function(user, callback) {
+        get_all_user_data : function (user, callback) {
             // Start with user Object
             this.get_user(user);
             // Then get user's repos list
             this.get_user_repos(
                 user,
                 // Then loop thru each repo
-                function(repos) {
-                    repos.forEach(function (repo, i) {
+                function (repos) {
+                    repos.forEach(function (repo) {
                         var i = 0;
                         // Get each individual repo data
                         github.get_repo(repo.owner.login, repo.name);
                         // Get each individual repo language
                         github.get_repo_lang(repo.owner.login, repo.name);
                         // Get each individual repo stat
-                        for (i in github.repo_stats) {
+                        for (i = 0; i < github.repo_stats.length; i += 1) {
                             github.get_repo_stat(repo.owner.login, repo.name, github.repo_stats[i]);
                         }
                     });
                 }
             );
             // Set up callback to execute when all ajax requests are done
-            $(document).ajaxStop(function() {
+            $(document).ajaxStop(function () {
                 console.log('All Requests complete!');
                 if (callback) {
                     callback(github.data.user, '');

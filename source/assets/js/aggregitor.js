ec6bb2e7ba0b25dfb6378d4eec8a4152a084c7a0
tmthydvnprt <tim@tmthydvnprt.com>
2016-02-17 04:37:28 -0800

    add aggregating object to take repo base data and combine

---
+++ source/assets/js/aggregitor.js
@@ -0,0 +1,28 @@
+/*globals $,console,d3,cookieJar,formatDate,github,opr,InstallTrigger,Blob */
+/*!
+ * aggregitor.js
+ *
+ * Copyright 2016 Timothy Davenport; Licensed MIT
+ */
+
+(function () {
+    'use strict';
+    window.aggregitor = {
+        user : null,
+        punch_card : function () {
+
+        },
+        code_frequency : function () {
+
+        },
+        commit_activity : function () {
+
+        },
+        contributors : function () {
+
+        },
+        participation : function () {
+
+        }
+    };
+}());

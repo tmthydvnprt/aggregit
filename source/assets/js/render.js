8c603f0f94f61810e568a76df5db7e6c4cb856c3
tmthydvnprt <tim@tmthydvnprt.com>
2016-02-20 08:25:26 -0800

    create render.js

---
+++ source/assets/js/render.js
@@ -0,0 +1,12 @@
+/*globals $,console,d3 */
+/*!
+ * render.js
+ *
+ * Copyright 2016 Timothy Davenport; Licensed MIT
+ */
+(function () {
+    'use strict';
+    window.renderUser = function (user) {
+
+    };
+}());
